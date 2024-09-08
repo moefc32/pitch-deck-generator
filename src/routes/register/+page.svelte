27828ac25cbb4564b10ade245018a290780b27e6
Faizal Chan <moefc32@gmail.com>
2024-09-09 05:35:06 +0700

    minor

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,4 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div
+  class="card flex flex-1 p-3 rounded-3xl shadow-lg bg-white border-2 border-gray-200"
+>
+  <div class="flex flex-1 flex-col justify-center items-center p-5">
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+      Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+    <p></p>
+  </div>
+  <div
+    class="image flex justify-center items-center rounded rounded-tr-2xl rounded-br-2xl bg-primary-500 w-3/5"
+  ></div>
+</div>
